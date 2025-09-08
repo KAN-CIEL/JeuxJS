73019cc729fe0f47c1cae47302f3a5b6993cf173
saiba kante <saibakante302@gmail.com>
2025-09-08 15:40:53 +0200

    #2 page html test

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+
 'use strict';
 
 console.log('TP CIEL');
+
+var express = require('express');
+
+var port = 80;
+
+var exp = express();
+
+exp.use(express.static(__dirname + '/WWW'));
+
+exp.ge('/', function (req, res) {
+    console.log('Reponse a un client');
+    res.sendFile(__dirname + '/WWW/index.html');
+});
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+
+exp.loisten(80, function () {
+    console.log('Serveur en ecoute');
+});
