32853b7f6e8ea87f9c114b626f10d453566f5e6e
saiba kante <saibakante302@gmail.com>
2025-09-08 16:34:18 +0200

    #4 méthode aWws.broascast

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ exp.use(express.static(__dirname + '/WWW'));
 
 exp.get('/', function (req, res) {
     console.log('Reponse a un client');
-    res.sendFile(__dirname + '/WWW/index.html');
+    res.sendFile(__dirname + '/WWW/textchat.html');
 });
 
 exp.use(function (err, req, res, next) {
@@ -33,9 +33,10 @@ exp.ws('/echo', function (ws, req) {
         req.connection.remoteAddress, req.connection.remotePort);
 
     ws.on('message', function (message) {
+        message = ws._socket._peername.address + ' : ' + message; 
         console.log('De %s %s, message :%s', req.connection.remoteAddress,
             req.connection.remotePort, message);
-        ws.send(message);
+        aWss.broadcast(message);
     });
 
     ws.on('close', function (reasonCode, description) {
@@ -44,6 +45,26 @@ exp.ws('/echo', function (ws, req) {
     });
 
 });
+
+/*  ****************** Broadcast clients WebSocket  **************   */
+var aWss = expressWs.getWss('/echo'); 
+var WebSocket = require('ws');
+aWss.broadcast = function broadcast(data) {
+    console.log('Broadcast aux clients navigateur : %s', data);
+    aWss.clients.forEach(function each(client) {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(data, function ack(error) {
+                console.log('    -  %s-%s', client._socket.remoteAddress,
+                    client._socket.remotePort);
+                if (error) {
+                    console.log('ERREUR websocket broadcast : %s', error.toString());
+                }
+            });
+        }
+    });
+};
+
+
 
 /*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
 //  
