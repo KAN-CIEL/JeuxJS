178635306978f507a1444e210d149d615a65393d
saiba kante <saibakante302@gmail.com>
2025-09-08 16:23:46 +0200

    #3 Websocket sur navigateur

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ console.log('TP CIEL');
 
 var express = require('express');
 
-var port = 80;
 
 var exp = express();
 
 exp.use(express.static(__dirname + '/WWW'));
 
-exp.ge('/', function (req, res) {
+exp.get('/', function (req, res) {
     console.log('Reponse a un client');
     res.sendFile(__dirname + '/WWW/index.html');
 });
@@ -21,6 +20,34 @@ exp.use(function (err, req, res, next) {
     res.status(500).send('Erreur serveur express');
 });
 
-exp.loisten(80, function () {
+
+
+/*  *************** serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
+});
+
+/*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
     console.log('Serveur en ecoute');
 });
